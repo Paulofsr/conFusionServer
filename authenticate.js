be8ddd7429a5cd8876879d31c586faa40bdb393d
Paulo Francisco <paulofsr@yahoo.com.br>
2020-01-04 03:09:02 -0200

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -37,3 +37,12 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyAdmin = (req, res, next) => {
+    if(req.user.admin)
+        next();
+    else{
+        var error = new Error('You are not to authorized to perform this operation!');
+        error.status = 403;
+        return next(error);
+    }
+}
