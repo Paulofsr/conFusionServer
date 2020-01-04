e660575e256a4ee4520068edc1679f00da8e2213
Paulo Francisco <paulofsr@yahoo.com.br>
2020-01-04 21:32:08 -0200

    File Upload

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const dishRouter = require('./routes/dishRouter');
-const promoRouter = require('./routes/promoRouter');
-const leaderRouter = require('./routes/leaderRouter');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -82,6 +83,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-var authenticate = require('../authenticate');
+const authenticate = require('../authenticate');
 
 const Dishes = require('../models/dishes');
 
